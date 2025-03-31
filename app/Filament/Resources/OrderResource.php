bfc3a76460e2b651bb762afe5b4997a8a3e36bb2
ditoma95 <devgtfod@gmail.com>
2025-03-31 17:09:50 +0000

    dlfjs

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -5,7 +5,15 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
+use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -13,17 +21,142 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use Filament\Forms\Set;
+use Filament\Forms\Get;
+
+use Illuminate\Support\Str;
+
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 //
+                Group::make()->schema([
+                    Section::make('Order Information')
+                        ->schema([
+                            Select::make('user_id')
+                                ->label('Customer')
+                                ->relationship('user', 'name')
+                                ->required()
+                                ->searchable()
+                                ->preload(),
+                            
+                            Select::make('payment_method')
+                                ->label('Payment Method')
+                                ->options([
+                                    'stripe' => 'Stripe',
+                                    'cod' => 'Cash on Delivery',
+                                ])
+                                ->required(),
+
+                            Select::make('payment_status')
+                                ->label('Payment Status')
+                                ->options([
+                                    'pending' => 'Pending',
+                                    'paid' => 'Paid',
+                                    'failed' => 'Failed',
+                                ])
+                                ->default('pending')
+                                ->required(),
+                            
+                            ToggleButtons::make('status')
+                                ->label('Status')
+                                ->inline()
+                                ->default('new')
+                                ->required()
+                                ->options([
+                                    'new' => 'New',
+                                    'processing' => 'Processing',
+                                    'shipped' => 'Shipped',
+                                    'delivered' => 'Delivered',
+                                    'cancelled' => 'Cancelled',
+                                ])
+                                ->colors([
+                                    'new' => 'info',
+                                    'processing' => 'warning',
+                                    'shipped' => 'success',
+                                    'delivered' => 'success',
+                                    'cancelled' => 'danger',
+                                ])
+                                ->icons([
+                                    'new' => 'heroicon-m-sparkles',
+                                    'processing' => 'heroicon-m-arrow-path',
+                                    'shipped' => 'heroicon-m-truck',
+                                    'delivered' => 'heroicon-m-check-badge',
+                                    'cancelled' => 'heroicon-m-x-circle',
+                                ]),
+                                Select::make('currency')
+                                    ->label('Currency')
+                                    ->options([
+                                        'inr' => 'INR',
+                                        'usd' => 'USD',
+                                        'eur' => 'EUR',
+                                        'gbp' => 'GBP',
+                                    ])
+                                    ->default('inr')
+                                    ->required(),
+
+                                Select::make('shipping_method')
+                                ->label('Shipping Method')
+                                ->options([
+                                    'fedex' => 'Fedex',
+                                    'dhl' => 'DHL',
+                                    'ups' => 'UPS',
+                                    'usps' => 'USPS',
+                                ])
+                                ->required(),
+
+                                Textarea::make('notes')
+                                ->label('Notes')
+                                ->columnSpanFull()
+                        ])->columns(2),
+
+                        Section::make('Order Items')->schema([
+                            Repeater::make('Items')
+                            ->relationship()
+                            ->schema([
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', Product::find($state)?->price ?? 0))
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', Product::find($state)?->price ?? 0))
+
+                                    ->columnSpan(4),
+                                
+                                TextInput::make('quantity')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount')))
+                                    ->columnSpan(2),
+
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->columnSpan(3),
+
+
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->columnspan(3),
+                            ])->columns(12),
+                        ])
+                ])->columnSpanFull()
             ]);
     }
 
