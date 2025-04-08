459b5d9220217c51d46ffaf6eb332c6732384b2e
ditoma95 <devgtfod@gmail.com>
2025-04-08 05:45:25 +0000

    dfljslk

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,101 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Models\Addresse;
+use App\Models\Order;
+use Illuminate\Support\Facades\Session;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Stripe\Stripe;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
 {
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $street_address;
+    public $city;
+    public $state;
+    public $zip_code;
+    public $payment_method;
+
+
+    public function placeOrder(){
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'street_address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required',
+        ]);
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $line_items = [];
+
+        foreach($cart_items as $item){
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'inr',
+                    'unit_amount' => $item['unit_amount'] * 100,
+                    'product_data' => [
+                        'name' => $item['name'],
+                    ]
+                    ],
+                    'quantity' => $item['quantity'],
+                ];
+        }
+        $order = new Order();
+        $order->user_id = auth()->user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $order->payement_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'inr';
+        $order->shipping_amount = 0;
+        $order->shopping_method = 'none';
+        $order->notes = 'Order place by ' . auth()->user()->name;
+
+
+        $address = new Addresse();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+        // $address->order_id = $order->id;
+        $redirect_url = '';
+
+        if ($this->payment_method == 'stripe') {
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $sessionCheckout = \Stripe\Checkout\Session::create([
+                'payment_method_types' => ['card'],
+                'customer_email' => auth()->user()->email, 
+                'line_items' => $line_items,
+                'mode' => 'payment',
+                'success_url' => route('success') . '?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route('cancel'),
+            ]);
+            $redirect_url = $sessionCheckout->url;
+        
+        }else{
+            $redirect_url = route('success');
+        }
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total
+        ]);
     }
 }
